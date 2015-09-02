3432b7aef93c9d0450248b9359bb8a52b486d899
theodortruffer <tt@studer-raimann.ch>
2015-09-02 10:46:43 +0200

    bugfix: could not select radiobutton at object creation if multiple plugins were activated

--- classes/class.ilOneDriveCreationGUI.php
+++ classes/class.ilOneDriveCreationGUI.php
@@ -10,10 +10,10 @@ require_once('./Modules/Cloud/classes/class.ilCloudPluginCreationGUI.php');
  */
 class ilOneDriveCreationGUI extends ilCloudPluginCreationGUI {
 
-	const F_BASE_FOLDER = 'base_folder';
-	const F_DEFAULT_BASE_FOLDER = 'default_base_folder';
-	const F_CUSTOM_FOLDER_SELECTION = 'custom_folder_selection';
-	const F_CUSTOM_BASE_FOLDER_INPUT = 'custom_base_folder_input';
+	const F_BASE_FOLDER = 'onedrive_base_folder';
+	const F_DEFAULT_BASE_FOLDER = 'onedrive_default_base_folder';
+	const F_CUSTOM_FOLDER_SELECTION = 'onedrive_custom_folder_selection';
+	const F_CUSTOM_BASE_FOLDER_INPUT = 'onedrive_custom_base_folder_input';
 
 
 	/**
@@ -22,14 +22,14 @@ class ilOneDriveCreationGUI extends ilCloudPluginCreationGUI {
 	public function initPluginCreationFormSection(ilRadioOption $option) {
 		//		$option->setInfo($this->txt('create_info1') . '</br>' . $this->txt('create_info2') . $this->getAdminConfigObject()->getAppName()
 		//			. $this->txt('create_info3'));
-		$sub_selection = new ilRadioGroupInputGUI($this->txt(self::F_BASE_FOLDER), self::F_BASE_FOLDER);
+		$sub_selection = new ilRadioGroupInputGUI($this->txt('base_folder'), self::F_BASE_FOLDER);
 		$sub_selection->setRequired(true);
 
-		$option_default = new ilRadioOption($this->txt(self::F_DEFAULT_BASE_FOLDER), self::F_DEFAULT_BASE_FOLDER);
+		$option_default = new ilRadioOption($this->txt('default_base_folder'), self::F_DEFAULT_BASE_FOLDER);
 
 		$option_custom = new ilRadioOption($this->txt('custom_base_folder'), self::F_CUSTOM_FOLDER_SELECTION);
 
-		$custom_base_folder_input = new ilTextInputGUI($this->txt(self::F_CUSTOM_BASE_FOLDER_INPUT), self::F_CUSTOM_BASE_FOLDER_INPUT);
+		$custom_base_folder_input = new ilTextInputGUI($this->txt('custom_base_folder_input'), self::F_CUSTOM_BASE_FOLDER_INPUT);
 		$custom_base_folder_input->setRequired(true);
 		$custom_base_folder_input->setInfo($this->txt('custom_base_folder_input_info'));
 
