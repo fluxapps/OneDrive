fad12b3601f00ab5fce655b4585cf0a63a34bc76
Stefan Wanzenried <stefan.wanzenried@gmail.com>
2016-06-01 09:46:25 +0200

    Support for ILIAS 5.1

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 $id = 'exod';
 $version = '0.0.5';
 $ilias_min_version = '4.4.0';
-$ilias_max_version = '5.0.999';
+$ilias_max_version = '5.1.999';
 $responsible = 'Fabian Schmid';
 $responsible_mail = 'fs@studer-raimann.ch';
 ?>
