1dce9f118e4c8e0645c780402195ed033e34aa4c
jdolf <jd@studer-raimann.ch>
2019-09-19 12:06:51 +0200

    [FIX] PLONEDRIVE-16 Kollaboratives Recht "In Office 365 öffnen"

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -56,3 +56,16 @@ $ilDB->manipulate(
         " auth_complete = 0"
 );
 ?>
+<#4>
+<?php
+//Adding a new Permission ("Open in Office Online")
+require_once("./Services/Migration/DBUpdate_3560/classes/class.ilDBUpdateNewObjectType.php");
+$orgu_type_id = ilDBUpdateNewObjectType::getObjectTypeId('cld');
+
+if ($orgu_type_id) {
+    $offering_admin = ilDBUpdateNewObjectType::addCustomRBACOperation(ilOneDrivePlugin::getInstance()->getPrefix() . '_asl_open_msoffice', 'open ms office', 'object', 280);
+    if ($offering_admin) {
+        ilDBUpdateNewObjectType::addRBACOperation($orgu_type_id, $offering_admin);
+    }
+}
+?>
