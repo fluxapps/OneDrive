d3268921c1527e1d94ef2c06ccb3b13692ec294d
Theodor Truffer <tt@studer-raimann.ch>
2020-02-19 16:42:28 +0100

    PLONEDRIVE-45

--- classes/Exception/ilOneDriveException.php
+++ classes/Exception/ilOneDriveException.php
@@ -12,6 +12,7 @@ class ilOneDriveException extends ilCloudException
     const ERROR_NAME_ALREADY_EXISTS = 'nameAlreadyExists';
 
     const MSG_ILLEGAL_CHARACTERS = 'The provided name cannot contain any illegal characters.';
+    const MSG_LEADING_OR_TRAILING_SPACES = 'The provided name cannot contain leading, or trailing, spaces.';
 
     /**
      * @param stdClass $error
@@ -27,6 +28,8 @@ class ilOneDriveException extends ilCloudException
                 switch ($error->message) {
                     case self::MSG_ILLEGAL_CHARACTERS:
                         return new self(self::RENAME_FAILED, ilOneDrivePlugin::getInstance()->txt('msg_illegal_characters'));
+                    case self::MSG_LEADING_OR_TRAILING_SPACES:
+                        return new self(self::RENAME_FAILED, ilOneDrivePlugin::getInstance()->txt('msg_leading_or_trailing_spaces'));
                     default:
                         return new self(self::UNKNONW_EXCEPTION, $error->message);
                 }
