ef771a23aedfe978b142d5a37dd44cd7abc4b95b
Theodor Truffer <tt@studer-raimann.ch>
2021-02-04 12:39:43 +0100

    composer & autoload

---
+++ src/Input/srChunkFileUploadInputGUI.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace srag\Plugins\OneDrive\Input;
+
+/**
+ * Class srChunkFileUploadInputGUI
+ * @package srag\DIC\OneDrive\Plugin
+ * @author  Theodor Truffer <tt@studer-raimann.ch>
+ */
+class srChunkFileUploadInputGUI
+{
+
+}
